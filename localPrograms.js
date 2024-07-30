aa6502500106c3504c0d19d35dbd7439192bb70c
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2024-07-29 22:53:28 -0600

    Program Details

---
+++ localPrograms.js
@@ -0,0 +1,43 @@
+
+const programs = [
+    { 
+        "name" : "Farm Team", 
+        "parentId" : "5d48728aeb91c400017a69b8",
+        "products" : ["5d48728aeb91c400017a69b8","66a7edca4409a764a4a614e0","66a7edca4ab7381a6683c20a","66a7edca4409a764a4a614e2","66a7edcb5947d02a66c77c9f","66a7edcb12d4a3603b16f54f","66a7edcb4ab7381a6683c20c","66a7edcb4ab7381a6683c20d","66a7edcb5947d02a66c77ca0","66a7edcc4409a764a4a614e3","66a7edcc4ab7381a6683c20f","66a7edcc5947d02a66c77ca1","66a7edcc5947d02a66c77ca2","66a7edcc4409a764a4a614e6","66a7edcd5947d02a66c77ca3","66a7edcd4409a764a4a614e7","66a7edcd12d4a3603b16f551","66a7edcd5947d02a66c77ca4","66a7edcd5947d02a66c77ca6","66a7edcd5947d02a66c77ca7","66a7edce12d4a3603b16f554","66a7edce5947d02a66c77ca8","66a7edce4ab7381a6683c213","66a7edce5947d02a66c77caa","66a7edce4409a764a4a614ea","66a7edcf4409a764a4a614eb","66a7edcf12d4a3603b16f556","66a7edcf12d4a3603b16f557",]
+    },
+    {   
+        "name" : "Learn To Race" , 
+        "parentId" : "5d48728aeb91c400017a69b6",
+        "products" : ["5d48728aeb91c400017a69b6","66a7edc74ab7381a6683c204","66a7edc84409a764a4a614dd","66a7edc812d4a3603b16f549","66a7edc812d4a3603b16f54a","66a7edc85947d02a66c77c99","66a7edc84ab7381a6683c207","66a7edc95947d02a66c77c9b","66a7edc95947d02a66c77c9c","66a7edc94409a764a4a614de","66a7edc912d4a3603b16f54c","66a7edc94ab7381a6683c209","66a7edca5947d02a66c77c9d","66a7edca12d4a3603b16f54d"],
+    },
+    {
+        "name" : "Junior Adventure - Sunday",
+        "parentId" : "62dae1cb98b31924d39bfb58", 
+        "products" : ["66a7eaf05947d02a66c77a49","66a7eaf14ab7381a6683bf9c","66a7eaf112d4a3603b16f334","66a7eaf14409a764a4a612b7","66a7eaf14409a764a4a612b8"]
+    },
+    {
+        "name" : "Junior Adventure - Saturday",
+        "parentId" : "62dae16f106f6c351393a959",
+        "products" : ["669fed275c2bb33e1246d5cb","669ff972ed1eb6407a9d4107","669ff9aca4ab3e7470696eab","669ff9d7a4ab3e7470696ed4","669ff9f0ed1eb6407a9d415a",]
+    },
+    {
+        "name" : "Women's Locals Program - Sunday",
+        "parentId" : "5d48728aeb91c400017a69b4",
+        "products" : ["66a7edc64ab7381a6683c1ff","66a7edc64ab7381a6683c200","66a7edc65947d02a66c77c96","66a7edc712d4a3603b16f546","66a7edc74ab7381a6683c202","66a7edc74ab7381a6683c203","66a7edc712d4a3603b16f547","66a7edc712d4a3603b16f548",]
+    },
+    {
+        "name" : "Women's Locals Program - Wednesday",
+        "parentId" : "62fd0e37c704c947d7e8e5d7",
+        "products" : ["66a7edc55947d02a66c77c91","66a7edc55947d02a66c77c92","66a7edc54ab7381a6683c1fc","66a7edc55947d02a66c77c93","66a7edc54409a764a4a614dc","66a7edc512d4a3603b16f542","66a7edc65947d02a66c77c94","66a7edc612d4a3603b16f543",]
+    },
+    {
+        "name" : "Program (Saturday Adventure, Mountain Team, AMP)",
+        "parentId" : "5d48728aeb91c400017a69ae",
+        "products" : ["66a7edc15947d02a66c77c89","66a7edc312d4a3603b16f53d","66a7edc15947d02a66c77c8a","66a7edc15947d02a66c77c8b","66a7edc14409a764a4a614d4","66a7edc24409a764a4a614d6","66a7edc24409a764a4a614d9","66a7edc24409a764a4a614da","66a7edc25947d02a66c77c8d","66a7edc25947d02a66c77c8e",]
+    },
+    {
+        "name" : "Program (Sunday Adventure, PC Ride, PeeWee)",
+        "parentId" : "5d48728aeb91c400017a69b2",
+        "products" : ["66a7edc35947d02a66c77c8f","66a7edc44ab7381a6683c1fa","66a7edc34409a764a4a614db","66a7edc34ab7381a6683c1f7","66a7edc312d4a3603b16f53e","66a7edc412d4a3603b16f53f","66a7edc44ab7381a6683c1f8","66a7edc412d4a3603b16f540","66a7edc412d4a3603b16f541","66a7edc44ab7381a6683c1f9",]
+    }
+] 
